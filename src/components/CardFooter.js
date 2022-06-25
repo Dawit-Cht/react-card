87753d3aeab859c9ea33538bba83c0f34994554e
Dawit-Cht <dawit.cht@gmail.com>
2022-06-25 11:32:31 +0700

    card footer

--- src/components/CardFooter.js
+++ src/components/CardFooter.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import Share from '../img/share.png'
+import User from '../img/user.png'
+import '../styles/cardFooter.css'
 
 export default function CardFooter() {
   return (
-    <div className='caard-footer'>
-      CardFooter
+    <div className="card-footer">
+      <div className="interest-count">
+        <img src={User} className="user"></img>
+        <p className="interest-text"> 10 คนที่สนใจ</p>
       </div>
+      <img src={Share} className="share"></img>
+    </div>
   )
 }
